88909b4ae3ae9dec26704caecd2222d8fe1bfc84
kylestubbs1234 <kylestubbs@ucsb.edu>
2021-01-19 03:42:05 -0800

    numbered each time variable, changed order of arguments for client

--- client.py
+++ client.py
@@ -5,9 +5,9 @@ import time
 import threading
 
 HOST = '127.0.0.1'
-PORT = int(sys.argv[1])
+MAX_DIFFERENCE = int(sys.argv[1])
 DRIFT = float(sys.argv[2])
-MAX_DIFFERENCE = float(sys.argv[3])
+PORT = float(sys.argv[3])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 current_time = datetime.now()
@@ -39,23 +39,23 @@ def call_server():
         time.sleep(MAX_DIFFERENCE/(2 * abs(DRIFT)))
 
         hour, minute, second = str(current_time.time()).split(":")
-        hour = int(hour)
-        minute = int(minute)
-        second = float(second)
+        hour1 = int(hour)
+        minute1 = int(minute)
+        second1 = float(second)
 
-        print("time before update is:","\nsecond: ", second, "\nminute: ", minute, "\nhour: ", hour, flush=True)
+        print("time before update is:","\nsecond: ", second1, "\nminute: ", minute1, "\nhour: ", hour1, flush=True)
         sock.send("request".encode())
         serverTime = sock.recv(1024).decode()
 
         hour, minute, second = str(serverTime).split(":")
         # temp, hour = hour.split(" ")
-        hour = int(hour)
-        minute = int(minute)
-        second = float(second)
+        hour2 = int(hour)
+        minute2 = int(minute)
+        second2 = float(second)
 
-        print("time after update is:","\nsecond: ", second, "\nminute: ", minute, "\nhour: ", hour, flush=True)
-        current_time = datetime(year=int(datetime.today().year), month=int(datetime.today().month), day=int(datetime.today().day), hour=hour, minute=minute)
-        current_time += timedelta(seconds=second)
+        print("time after update is:","\nsecond: ", second2, "\nminute: ", minute2, "\nhour: ", hour2, flush=True)
+        current_time = datetime(year=int(datetime.today().year), month=int(datetime.today().month), day=int(datetime.today().day), hour=hour2, minute=minute2)
+        current_time += timedelta(seconds=second2)
 
 def Main():
     sock.connect((HOST, PORT))
